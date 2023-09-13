ed24ad7925517686c9198ca8d72acbd06e821965
Zakaria El bouzkri <elbouzkri9@gmail.com>
2023-09-13 07:27:50 +0100

    in progresss...

--- Module03/ex00/ClapTrap.cpp
+++ Module03/ex00/ClapTrap.cpp
@@ -5,70 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 21:00:44 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/09/11 21:30:38 by zel-bouz         ###   ########.fr       */
+/*   Created: 2023/09/13 07:12:09 by zel-bouz          #+#    #+#             */
+/*   Updated: 2023/09/13 07:27:18 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "ClapTrap.hpp"
 
 
-ClapTrap::ClapTrap(std::string name): _name(name){
-	std::cout << "Default constructor called\n";
-	_energyPoints = 10;
-	_attackDamage = 0;
-	_hitPoints = 10;
+ClapTrap::ClapTrap( void ) : _hitPoints(10),
+	_energyPoints(10), _attackDamage(0){
+		std::cout << "The default constructor called\n";
+}
+
+ClapTrap::ClapTrap( std::string name ) : _name(name),
+	_hitPoints(10), _energyPoints(10), _attackDamage(0){
+		std::cout << "The name constructor called\n";
 }
 
 
-ClapTrap::~ClapTrap(){
-	std::cout << "the destructor called\n";
+ClapTrap::~ClapTrap( void ){
+	std::cout << "The destructor called\n";
 }
 
 
-ClapTrap::ClapTrap(ClapTrap const &rhs){
-	std::cout << "copy constructor called\n";
-	if (this !=  &rhs){
-		this->_name = rhs._name;
-		this->_hitPoints = rhs._hitPoints;
-		this->_energyPoints = rhs._energyPoints;
-		this->_attackDamage = rhs._attackDamage;
-	}
+ClapTrap::ClapTrap( ClapTrap const &rhs ) : _name(rhs._name), _hitPoints(rhs._hitPoints),
+	_energyPoints(rhs._energyPoints), _attackDamage(rhs._attackDamage){
+		std::cout << "the copy constructor called\n";
 }
 
 
-ClapTrap	&ClapTrap::operator=(const ClapTrap &rhs){
-	std::cout << "copy assignment operator overload called\n";
+ClapTrap	&ClapTrap::operator=( const ClapTrap &rhs ){
+	std::cout << "the copy asignment operator overload called\n";
 	if (this != &rhs){
-		this->_name = rhs._name;
-		this->_hitPoints = rhs._hitPoints;
-		this->_energyPoints = rhs._energyPoints;
-		this->_attackDamage = rhs._attackDamage;
+		_name = rhs._name;
+		_hitPoints = rhs._hitPoints;
+		_energyPoints = rhs._energyPoints;
+		_attackDamage = rhs._attackDamage;
 	}
 	return (*this);
 }
 
-void	ClapTrap::attack(const std::string& target){
-	if (_hitPoints > 0 && _energyPoints > 0){
-		_energyPoints--;
-		std::cout << "ClapTrap " << _name << " attacks " << target
-		<< " causing " << _attackDamage << " points of damage\n";
-	}
-}
 
-void	ClapTrap::takeDamage(unsigned int amount){
-	if (_hitPoints > 0 && _energyPoints > 0){
-		_hitPoints -= amount;
-		_energyPoints--;
-		std::cout << "take damage memeber func called\n";
-	}
-}
-
-void	ClapTrap::beRepaired(unsigned int amount){
-	if (_hitPoints > 0 && _energyPoints > 0){
-		_energyPoints--;
-		_hitPoints += amount;
-		std::cout << "ClapTrap " << _name << " is Repaired\n";
-	}
-}
--- Module03/ex00/ClapTrap.hpp
+++ Module03/ex00/ClapTrap.hpp
@@ -5,33 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 20:55:58 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/09/11 20:59:52 by zel-bouz         ###   ########.fr       */
+/*   Created: 2023/09/13 07:12:07 by zel-bouz          #+#    #+#             */
+/*   Updated: 2023/09/13 07:18:06 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _ClapTrap_HPP_
 #define _ClapTrap_HPP_
 
+
 #include <iostream>
+
 
 class	ClapTrap
 {
 	private:
-		std::string _name;
+		std::string	_name;
 		int			_hitPoints;
 		int			_energyPoints;
 		int			_attackDamage;
 	public:
-		ClapTrap(std::string name);
-		~ClapTrap();
-		ClapTrap(ClapTrap const &obj);
-		ClapTrap	&operator=(const ClapTrap &obj);
-		void		attack(const std::string& target);
-		void		takeDamage(unsigned int amount);
-		void		beRepaired(unsigned int amount);
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		~ClapTrap( void );
+		ClapTrap( ClapTrap const &obj );
+		ClapTrap &operator=( const ClapTrap &obj );
+		void	attack( const std::string& target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
 };
-
-
 
 #endif
