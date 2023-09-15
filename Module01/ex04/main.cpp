75f5639ef82484cb1d97636596810b96a70d747e
Zakaria El bouzkri <elbouzkri9@gmail.com>
2023-09-15 23:54:33 +0100

    fix sedforlosers

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -3,48 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 06:52:44 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/09/15 20:05:48 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/15 23:52:45 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <iostream>
 #include <fstream>
-#include <string.h>
 
+void	usage( std::string program ){
+	std::cout << "usage: " << program << " ";
+	std::cout << "[filename]" << " ";
+	std::cout << "[toFind]" << " ";
+	std::cout << "[toReplace]" << "\n";
+}
 
-int	sedForLosers(std::string filename, std::string toFind, std::string toReplace){
-	std::ifstream	inFile(filename.c_str());
-	std::ofstream	outFile("outFile.txt");
+std::string	sTrim(std::string&	str){
+	std::string	newStr;
+
+	size_t	start = str.find_first_not_of(" \v\r\f\t\n");
+	size_t	end = str.find_last_not_of(" \v\r\f\t\n");
+	newStr = str.substr(start, end-start+1);
+	return (newStr);
+}
+
+bool	isValid(std::string filename, std::string toFind, std::string toReplace){
+	if (filename.empty() || toFind.empty() || toReplace.empty()) return (false);
+	if (sTrim(toFind) == sTrim(toReplace)) return (false);
+	return (true);
+}
+
+bool	sedForLosers(std::string filename, std::string toFind, std::string toReplace){
+	if (!isValid(filename, toFind, toReplace)){
+		return (false);
+	}
+	std::string		out = filename + ".replace";
+	std::ifstream	infile(filename);
+	std::ofstream	outfile(out);
 	std::string		line;
 	size_t			pos;
-	if (!inFile.is_open() || !outFile.is_open()){
-		std::cerr << strerror(errno) << "\n";
-		return (1);
+	if (!outfile.is_open() || !infile.is_open()){
+		(outfile.close(), infile.close());
+		return (false);
 	}
-	while (getline(inFile, line)){
-		while (toFind != toReplace && (pos = line.find(toFind)) != std::string::npos){
+	while (getline(infile, line))
+	{
+		while ((pos = line.find(toFind)) != std::string::npos){
 			line.erase(pos, toFind.size());
 			line.insert(pos, toReplace);
 		}
-		outFile << line << "\n";
+		outfile << line << "\n";
 		line.clear();
 	}
-	inFile.close();
-	outFile.close();
-	return (0);
+	(outfile.close(), infile.close());
+	return (true);
 }
 
+
 int main(int ac, char **av){
-	if (ac != 4){
-		std::cout << "usage: " << av[0] << " ";
-		std::cout << "[filename]" << " ";
-		std::cout << "[toFind]" << " ";
-		std::cout << "[toReplace]" << "\n";
+	if (ac != 4) return (usage(av[0]), 1);
+	if (!sedForLosers(av[1], av[2], av[3])){
+		std::cout << "Error\n";
 		return (1);
 	}
-	return (sedForLosers(av[1], av[2], av[3]));
 }
